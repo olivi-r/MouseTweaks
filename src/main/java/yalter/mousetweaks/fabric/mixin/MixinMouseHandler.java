b7fedc6e3d23a07521f746c069843d71b5afbdcc
Olivia Ryan <olivia.r.dev@gmail.com>
2025-10-02 11:36:45 +0100

    Rename mouseButtonEvent to event

--- src/main/java/yalter/mousetweaks/fabric/mixin/MixinMouseHandler.java
+++ src/main/java/yalter/mousetweaks/fabric/mixin/MixinMouseHandler.java
@@ -15,14 +15,14 @@ import yalter.mousetweaks.MouseButton;
 @Mixin(MouseHandler.class)
 public abstract class MixinMouseHandler {
     @WrapOperation(method = "handleAccumulatedMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;mouseDragged(Lnet/minecraft/client/input/MouseButtonEvent;DD)Z"))
-    private boolean onMouseDragged(Screen screen, MouseButtonEvent mouseButtonEvent, double dx, double dy, Operation<Boolean> operation) {
-        MouseButton button = MouseButton.fromEventButton(mouseButtonEvent.button());
+    private boolean onMouseDragged(Screen screen, MouseButtonEvent event, double dx, double dy, Operation<Boolean> operation) {
+        MouseButton button = MouseButton.fromEventButton(event.button());
         if (button != null) {
-            if (Main.onMouseDrag(screen, mouseButtonEvent.x(), mouseButtonEvent.y(), button)) {
+            if (Main.onMouseDrag(screen, event.x(), event.y(), button)) {
                 return true;
             }
         }
 
-		return operation.call(screen, mouseButtonEvent, dx, dy);
+		return operation.call(screen, event, dx, dy);
 	}
 }
